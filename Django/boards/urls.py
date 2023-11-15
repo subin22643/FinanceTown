7c09ac1481c0722333ff2be2f0e656fe63bf3ad8
subin22643 <bin577733@naver.com>
2023-11-15 16:13:15 +0900

    예적금django

--- Django/boards/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "boards"
-
-urlpatterns = [
-    path('boards/', views.boards, name="boards"),
-    path('boards/<int:pk>', views.boardDetail, name="boardDetail"),
-]

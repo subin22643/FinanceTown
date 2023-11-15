b008ca6e0656fefb56b59edaf6fe0aa1776fde8d
Jeongjonggil <whdrlf3928@naver.com>
2023-11-15 16:04:00 +0900

    jonggilvue

---
+++ Vue/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+    <div class ="ms-4 mt-3">
+      <h1>메인 페이지</h1>
+      <RouterLink :to="{ name: 'search' }">조회</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'map' }">지도</RouterLink>
+      <RouterView/>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
